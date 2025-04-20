d5d0580eed41dd38b2bb4aba8b17dc4940921c6b
Victor Chandrasekar <victorroshan4@gmail.com>
2025-04-20 16:43:37 +0530

    frontend url

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://job-portal-frontend-orcin-omega.vercel.app/'], // Your frontend URL
+    origin: [ 'https://job-portal-frontend-orcin-omega.vercel.app'], // Your frontend URL
     credentials: true
   }));
+  
 app.use(express.json());
 
 // Connect to MongoDB
