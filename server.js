5eb82009b5aa073a8b2ad5bef46814af781cb99f
Victor Chandrasekar <victorroshan4@gmail.com>
2025-04-20 17:31:49 +0530

    Fix CORS configuration

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: [ 'https://job-portal-frontend-orcin-omega.vercel.app']
-  }));
+  origin: ['https://job-portal-frontend-orcin-omega.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 
 app.use(express.json());
 
